2a27c6d1a998a5ec4c616922942153e03aa4609a
Jashele T <jashele@Jasheles-Air.domain>
2019-11-13 20:28:50 -0700

    get by ID is working, postman

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -15,4 +15,24 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("accounts")
+    .select("*")
+    .where({ id })
+    .then(account => {
+      if (account[0]) {
+        res.status(200).json(account);
+      } else {
+        res.status(404).json({ message: "Invalid ID" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        Message: "Database Problem",
+        err
+      });
+    });
+});
+
 module.exports = router;
