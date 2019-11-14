2a947971e3792462bb0e7f76bfd1cf47a25c8740
Jashele T <jashele@Jasheles-Air.domain>
2019-11-13 19:55:45 -0700

    setup router and port is listening

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const AccountRouter = require("./accounts/accounts-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", AccountRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h3>Accounts API - Jashele T. </h3>");
+});
 
 module.exports = server;
